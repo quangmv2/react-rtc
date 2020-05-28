f619d0671189d127e5c016f601a325407af26134
Mai Van Quang <maiquang1470@gmail.com>
2020-05-28 09:31:07 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect, createRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Peer from "peerjs";
+
+const peer = new Peer(null, {
+  host: '192.168.10.123',
+  port: 9000,
+  path: '/peer-server'
+});
+const getMedia = config => {
+  return navigator.mediaDevices.getUserMedia(config);
+}
 
 function App() {
+
+  const [idPeer, setIdPeer] = useState(null);
+  const [audio, setAudio] = useState(false);
+  const [idCall, setIdCall] = useState(null);
+  const video = createRef(null);
+
+  useEffect(() => {
+    peer.on('open', id => {
+      setIdPeer(id);
+    })
+  }, []);
+
+  const idCallChange = function(e) {
+    setIdCall(e.target.value);
+  }
+
+  const callPeer = (id, stream) => {
+    const call = peer.call(id, stream);
+    call.on('stream', rmStream => {
+      video.current.srcObject = rmStream;
+    })
+  }
+
+  const clickCall = () => {
+    getMedia({
+      audio: true,
+      video: true
+    }).then(stream => {
+      callPeer(idCall, stream);
+    }).catch(err => {
+      console.log(err);
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Id peer: {idPeer?idPeer:''}</p>
+      <input onChange={idCallChange}/>
+      <button
+        onClick={clickCall}
+      >Call</button>
+      <video autoPlay ref={video}></video>
     </div>
   );
 }
